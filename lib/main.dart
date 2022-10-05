dcbb8d0efda454dd5647d3c2558edbe7e475b6c0
Jawher Alkotaim <jawheralkotaim@Jawhers-MacBook-Pro.local>
2022-10-05 12:10:52 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,98 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ToDoListApp());
+void main() => runApp(const MyApp());
 
-class ToDoListApp extends StatelessWidget {
-  const ToDoListApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AppBarExample(),
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget {
-  const AppBarExample({super.key});
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+  final List<String> inputs = [];
+  late String input ;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tasks"),
+        backgroundColor: Colors.indigoAccent.withOpacity(0.5),
+        title: const Text('To Do'),
       ),
-      body: AppBody(),
-    );
-  }
-}
+      body: Center(
+        child: Column(
+          children: [
+             Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                onChanged: (String value) {
+                  input=value;
+                  print(input);},
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a task',
+                ),
+              ),
+            ),
+            ...inputs.map((input) => ListItem(input)),
+          ],)
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(height: 50.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigoAccent.withOpacity(0.5),
+        onPressed: () => setState(() {
+          print("button is clicked");
+          inputs.add(input);
+          _count++;
+        }),
+        tooltip: 'Increment Counter',
+        child:
+          const Icon(Icons.add, color:Colors.white,),
 
-class AppBody extends StatelessWidget {
-  const AppBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListItem(),
-        ListItem()
-      ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
 
 class ListItem extends StatelessWidget {
-  const ListItem({super.key});
+  String data;
+  ListItem(this.data);
+
 
   @override
   Widget build(BuildContext context) {
     return  Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.only(bottom: 12),
-        child: Container(
-               child: Text("tasks"),
-               padding: EdgeInsets.all(16),
-               decoration: BoxDecoration(
-               borderRadius: BorderRadius.circular(8),
-               color: Colors.indigoAccent,),
-        ),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(bottom: 12),
+      child: Container(
+        child: Text(data),
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.indigoAccent.withOpacity(0.5),),
+      ),
     );
   }
 }
