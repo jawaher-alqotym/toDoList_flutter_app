96db8bac27e5a8b5a47170dcad483f4c6becaede
Jawher Alkotaim <jawheralkotaim@Jawhers-MacBook-Pro.local>
2022-10-05 09:14:12 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const AppBarApp());
+void main() => runApp(const ToDoListApp());
 
-class AppBarApp extends StatelessWidget {
-  const AppBarApp({super.key});
+class ToDoListApp extends StatelessWidget {
+  const ToDoListApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,52 +22,43 @@ class AppBarExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Master'),
-          backgroundColor: Colors.amber,
-          titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
-          actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            color: Colors.black,
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            color: Colors.black,
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Next page'),
-                      backgroundColor: Colors.amber,
-                      titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
+        title: Text("Tasks"),
+      ),
+      body: AppBody(),
+    );
+  }
+}
 
-                    ),
-                    body: const Center(
-                      child: Text(
-                        'This is the next page',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  );
-                },
-              ));
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'This is the home page',
-          style: TextStyle(fontSize: 24),
+class AppBody extends StatelessWidget {
+  const AppBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListItem(),
+        ListItem()
+      ],
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.only(bottom: 12),
+        child: Container(
+               child: Text("tasks"),
+               padding: EdgeInsets.all(16),
+               decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(8),
+               color: Colors.indigoAccent,),
         ),
-      ),
     );
   }
 }
