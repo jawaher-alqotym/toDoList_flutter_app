700f0419f3df52628e4eaf9d5088b25c730cf388
Jawher Alkotaim <jawheralkotaim@Jawhers-MacBook-Pro.local>
2022-10-05 15:31:33 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// lib/main
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -23,11 +25,17 @@ class MyStatefulWidget extends StatefulWidget {
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _count = 0;
-  final List<String> inputs = [];
-  late String input ;
 
+void removeItem(item){
+  print(inputs);
+  inputs.remove(item);
+  print(inputs);
+
+}
+
+final List<String> inputs = [];
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  late String input;
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +45,24 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         title: const Text('To Do'),
       ),
       body: Center(
-        child: Column(
-          children: [
-             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                onChanged: (String value) {
-                  input=value;
-                  print(input);},
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a task',
-                ),
+          child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              onChanged: (String value) {
+                input = value;
+                //print(input);
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a task',
               ),
             ),
-            ...inputs.map((input) => ListItem(input)),
-          ],)
-      ),
+          ),
+          ...inputs.map((input) => ListItem(input)),
+        ],
+      )),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
         child: Container(height: 50.0),
@@ -61,37 +70,99 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.indigoAccent.withOpacity(0.5),
         onPressed: () => setState(() {
-          print("button is clicked");
+          //print("button is clicked");
           inputs.add(input);
-          _count++;
         }),
-        tooltip: 'Increment Counter',
-        child:
-          const Icon(Icons.add, color:Colors.white,),
-
+        child: const Icon(
+          Icons.add_task,
+          color: Colors.white,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
 
-class ListItem extends StatelessWidget {
+class ListItem extends StatefulWidget {
   String data;
   ListItem(this.data);
 
+  @override
+  State<ListItem> createState() => _ListItemState();
+}
+
+class _ListItemState extends State<ListItem> {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       padding: EdgeInsets.symmetric(horizontal: 20),
       width: MediaQuery.of(context).size.width,
       margin: EdgeInsets.only(bottom: 12),
       child: Container(
-        child: Text(data),
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.indigoAccent.withOpacity(0.5),),
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.indigoAccent.withOpacity(0.5),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  widget.data,
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              Expanded(child: Container(
+                height: 60.0,
+                width: 60.0,
+              ),),
+              Expanded(
+                child: SizedBox(
+                  height: 65,
+                  width: 65,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(
+                        Icons.edit,
+                        color: Colors.grey ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      elevation: 0.0,
+                      shadowColor: Colors.transparent,
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(24),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: SizedBox(
+                  height: 65,
+                  width: 65,
+                    child: ElevatedButton(
+                    onPressed: () {
+                      print(widget.data);
+                      removeItem(widget.data);
+                    },
+                       child: const Icon(
+                       Icons.delete,
+                       color: Colors.redAccent ),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    elevation: 0.0,
+                    shadowColor: Colors.transparent,
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.all(24),
+                  ),
+                ),
+               ),
+              ),
+            ],
+          ),
       ),
     );
   }
